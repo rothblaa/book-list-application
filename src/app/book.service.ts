931ce6df130008f09609cf5b3d49ebedaa7b3c0c
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 03:03:46 -0500

    build book service, send get request with obs to api

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,11 +1,21 @@
+import { Book } from './book';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private apiUrl = '';
+  //not good practice to put this here, but just for simplicity of small app....
+  private apiUrl = 'https://localhost:7148';
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  // returns obs of type book array; gets value asynchronously after subscribing to obs
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
 }
