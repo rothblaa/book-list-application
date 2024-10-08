5451d6cccea1afe345333ec0648fd0e17f3d2349
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 01:31:53 -0500

    scaffolding for template set up

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Book } from './../book';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
-
+  books: Book[] = [
+    {id: 1, title: "Book One", author: "Author One"},
+    {id: 2, title: "Book Two", author: "Author Two"},
+    {id: 3, title: "Book Three", author: "Author Three"}
+  ]
 }
